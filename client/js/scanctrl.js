f8d6a1ac50214f57141b325fcabc1c3416840414
Frederik B <fbraun+gh@mozilla.com>
2014-02-25 16:14:18 +0100

    adding codemirror for file uploads

--- client/js/scanctrl.js
+++ client/js/scanctrl.js
@@ -14,6 +14,7 @@ getJSON('jquery', 'js/lib/tern/defs/jquery.json');
 
 var scanjsModule=angular.module('scanjs', ['ui.bootstrap']);
 function ScanCtrl($scope, ScanSvc) {
+  $scope.codeMirror = undefined;
 
   $scope.sourceinput = 'dangerous.innerHTML=document.location;\n' + 
   'element[\'innerHTML\']=something+"we can catch calls to literal members;"\n' + 
@@ -47,24 +48,46 @@ function ScanCtrl($scope, ScanSvc) {
     return (item.length>0)
   }
 
-  $scope.handleFiles = function handleFiles(fileList) {
+  $scope.handleFileUpload = function handleFileUpload(fileList) {
     for (var i = 0; i < fileList.length; i++) { // This should really just take 1 file
       var file = fileList[i];
       console.log("File: ", file.name, " Filetype: "+ file.type);
-      var acceptedType = /(application\/json)|(text\/plain)/;
-      if (!file.type.match(acceptedType)) {
-        $scope.error = "Invalid file type, expected JSON.";
+      var jsonType = /(application\/json)/;
+      var jsType = /(text\/javascript|application\/javascript|application\/x-javascript)/;
+      var curFileType;
+      if (file.type.match(jsType)) {
+        curFileType = "js"
+      } else if (file.type.match(jsonType)) {
+        curFileType = "json"
+      } else {
+        $scope.error = "Error: Unknown file type. Expected JavaScript or JSON.";
         $scope.$apply();
         break;
       }
       var reader = new FileReader();
       reader.onload = function(e) {
-        try {
-          var resultObj = JSON.parse(e.target.result);
-          ScanSvc.addResults(resultObj);
-        } catch(e) { // gotta catch them all
-          $scope.error = "Could not parse JSON";
-          $scope.$apply();
+        if (curFileType == "json") {
+          try {
+            var resultObj = JSON.parse(e.target.result);
+            ScanSvc.addResults(resultObj);
+          } catch(e) { // gotta catch them all
+            $scope.error = "Could not parse JSON";
+            $scope.$apply();
+          }
+        } else if (curFileType == "js") {
+          // code mirror stuff
+          if ($scope.codeMirror === undefined) {
+          $scope.codeMirror = new CodeMirror(function(elt) {
+                                                             var ta=document.getElementById('jsTextarea');
+                                                             ta.parentNode.replaceChild(elt, ta);
+                                              }, { mode: 'javascript', lineNumbers: true, readOnly: false,
+                                              value: e.target.result});
+          } else {
+            $scope.codeMirror.setValue(e.target.result);
+          }
+
+          $scope.sourceinput = e.target.result;
+          $scope.run();
         }
       };
       reader.onerror = function(e) {
