87c47130d4df38bae84debe7278c8dd03c9b1250
Frederik B <fbraun+gh@mozilla.com>
2014-04-11 14:50:46 +0200

    make hosting scanjs web ui a stackato task. this changes server.js to host "client/" instead of "."

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 var static = require("node-static");
-var file = new static.Server('.', {
+var file = new static.Server('./client', {
   headers: {
     "Content-Security-Policy": "default-src 'self'; object-src 'none'; img-src 'self' data:; script-src 'self' 'unsafe-eval'",
   }
 });
 
+const PORT = process.env.PORT || 4000;
 require ('http').createServer(function (req, res) {
   req.addListener('end', function () {
     file.serve(req, res);
   }).resume();
-}).listen(4000);
+}).listen(PORT);
 
-console.log("> node-static is listening on http://127.0.0.1:4000");
+console.log("> node-static is listening on http://127.0.0.1:"+PORT);
